733021e4b503060db236c33a742265f952ebf993
ji798 <jisuazom@uamv.edu.ni>
2026-09-04 08:09:28 -0600

    agregando funciones2.py

---
+++ funciones/funciones2.py
@@ -0,0 +1,10 @@
+#Sumar 2 numeros
+
+def sumar(numero1, numero2):
+
+    return numero1 + numero2
+
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+
+print("La suma de los numeros es: ", sumar(numero1, numero2))

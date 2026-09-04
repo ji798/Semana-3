73ae701708df0cdca26b3356ccbc06fcd9291eae
ji798 <jisuazom@uamv.edu.ni>
2026-09-04 08:17:18 -0600

    Create funciones2.py

---
+++ funciones/funciones2.py
@@ -0,0 +1,17 @@
+#Sumar 2 numeros
+
+def sumar(numero1 , numero2):
+
+    return numero1 + numero2
+
+suma = sumar(15, 17)
+print(suma)
+
+def restar(num1 = 0 , num2 = 0):
+    return num1 - num2
+
+resta = restar(4, 2)
+print(resta)
+
+resta = restar(num2= 6, num1=2)
+print(resta)
